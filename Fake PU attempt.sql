5db81c73c127807ce0f9842efefde2542e6953d2
aaron996 <35775571+aaron996@users.noreply.github.com>
2025-10-14 17:36:09 +0700

    Create Fake PU attempt.sql

---
+++ Fake PU attempt.sql
@@ -0,0 +1,111 @@
+WITH
+ pku_done_tracking AS (
+   SELECT
+    slo_id log_id
+   , min(case when tracking_code in ('F100','F510') then from_unixtime((CASE WHEN (ctime > actual_time) THEN (actual_time - 3600) ELSE (ctime - 3600) END)) end ) pku_done_time
+   , min(case when tracking_code in ('F001','F002','F100','F510','F050','F097','F098') then from_unixtime((CASE WHEN (ctime > actual_time) THEN (actual_time - 3600) ELSE (ctime - 3600) END)) end ) pku_1st_time
+    , min(case when tracking_code in ('F097') then from_unixtime((CASE WHEN (ctime > actual_time) THEN (actual_time - 3600) ELSE (ctime - 3600) END)) end ) pku_failed_time 
+   FROM
+    sls_mart.shopee_ssc_lts_tracking_vn_db__logistic_tracking_tab__reg_continuous_s0_live 
+   GROUP BY 1
+)
+, Final AS (
+   SELECT distinct t3.order_id
+   , t1.lm_tracking_no
+   , from_unixtime(coalesce(rd.pickup_time,t1.scheduled_pickup_timestamp)) scheduled_date
+   , pku_done_time
+   , pku_failed_time
+   , t1.fulfillment_channel_id
+   ,t3.order_arrange_shipping_timestamp Logistics_request_created
+   , CASE WHEN (year(from_unixtime(coalesce(rd.pickup_time,t1.scheduled_pickup_timestamp))) = 1970) THEN from_unixtime(t3.order_arrange_shipping_timestamp)
+            ELSE from_unixtime(coalesce(rd.pickup_time,t1.scheduled_pickup_timestamp)) END as scheduled_date1
+    ,t5.pku_1st_time
+       , (CASE --when t1.inbound_type = 1 then 'pickup' 
+            --when t1.inbound_type = 2 then 'drop-off'
+                                    WHEN year(from_unixtime(rd.pickup_time-3600)) > 1970 THEN 'pickup'
+                                    WHEN (year(from_unixtime(rd.pickup_time-3600)) = 1970 AND year(from_unixtime(t3.order_arrange_shipping_timestamp-3600)) > 1970) THEN 'drop-off'
+                                    ELSE NULL
+                                END) 
+                                shipment_type
+    , case when t1.inbound_type = 1 then 'pickup'
+          when t1.inbound_type = 2 then 'dropoff'
+          else null end  shipment_type1
+    , (CASE WHEN ((year(from_unixtime(t1.scheduled_pickup_timestamp)) > 1970) AND (t1.scheduled_pickup_timestamp IS NOT NULL)) THEN 1 ELSE 0 END) gross_pickup_request
+    , (CASE WHEN (date(pku_done_time) <= date(from_unixtime(t1.scheduled_pickup_timestamp))) THEN 1 ELSE 0 END) ontime_pickup_done
+    , (CASE WHEN (date(pku_1st_time) <= date(from_unixtime(t1.scheduled_pickup_timestamp))) THEN 1 ELSE 0 END) ontime_pickup
+    , (CASE WHEN ((year(from_unixtime(t1.scheduled_pickup_timestamp)) > 1970) AND (t1.scheduled_pickup_timestamp IS NOT NULL) and pku_done_time is not null) THEN 1 ELSE 0 END) gross_pickup_done
+    , (CASE WHEN pku_failed_time is not null THEN 1 ELSE 0 END) gross_pickup_failed
+    , case
+    when t1.log_status = 1000 then 'NEW'
+    when t1.log_status in (1001,1040) then 'INFO_RECEIVED'
+    when t1.log_status in (1061,1051,2001,2008,2009,2015,2030,2040,3001,3060,3080,3090) then 'IN_TRANSIT'
+    when t1.log_status in (9997,3099,3070) then 'EXCEPTION'
+    when t1.log_status = 9999 then 'CANCELLED'
+    when t1.log_status = 1050 then 'DOMESTIC_PICKUP'
+    when t1.log_status in  (4010,4020,4100) then 'DOMESTIC_SORTING'
+    when t1.log_status = 4060 then 'DOMESTIC_DELIVERING'
+    when t1.log_status = 4097 then 'DOMESTIC_DELIVERED'
+    when t1.log_status = 4070 then 'DELIVERY_PENDING'
+    when t1.log_status = 9998 then 'PARCEL_LOST'
+    when t1.log_status =  8020 then 'PARCEL_RETURN'
+    when t1.log_status = 8050 then 'RETURN_COMPLETED'
+    when t1.log_status = 8099 then 'RETURN_FAILED'
+    when t1.log_status = 8045 then 'PENDING_FOR_RETURN'
+    when t1.log_status in (4098,8001,4102) then 'REQUEST_RETURN'
+    when t1.log_status in (996,1041,8010) then 'PICKUP_RETRY'
+    when t1.log_status in (1099,2099,9995) then 'DOMESTIC_PICKUP_FAILED'
+    when t1.log_status = 1060 then 'DOMESTIC_DROPOFF'
+    when t1.log_status = 1042 then 'DOMESTIC_PICKUP_PENDING'
+    when t1.log_status in (8000,8040) then 'RETURN_STARTED'
+    when t1.log_status = 4101 then 'UPDATE_STORE_SUBMITTED'
+    when t1.log_status = 4103 then 'UPDATE_STORE_CANCEL'
+    when t1.log_status = 9994 then 'IMLM_DISPOSED'
+    when t1.log_status = 8060 then 'PARCEL_CONFISCATED'
+    when t1.log_status = 9993 then 'TERMINATE_AS_DAMAGE'
+    else 'no'
+  end as sls_status
+   FROM sls_mart.dwd_ofg_forder_union_df_vn t3
+    left join sls_mart.dwd_parcel_detail_union_nonsensitive_df_vn t1 on t1.grass_date = current_date - interval '1' day and t1.log_id = t3.log_id
+   LEFT JOIN pku_done_tracking t5 ON (t5.log_id = t3.log_id)
+   left join sls_mart.shopee_ssc_lfs_order_vn_db__logistic_order_data_tab__reg_continuous_s0_live rd on  rd.slo_id = t3.log_id
+
+)
+SELECT
+--  week(scheduled_date1)+1 week_
+o.fulfilment_shipping_carrier
+--, f.lm_tracking_no
+--, o.order_sn
+,o.seller_shipping_address_state
+, o.seller_shipping_address_city
+--, o.seller_shipping_address_district
+--, o.cancel_reason
+--, o.buyer_cancel_reason
+--, o.logistics_status
+--, o.order_be_status
+--, f.sls_status
+--, o.shop_id
+--, o.shop_name
+--, o.buyer_id
+--, f.scheduled_date1
+--, f.pku_1st_time
+--, date(f.scheduled_date1)
+--, coalesce(f.shipment_type,f.shipment_type1) shipment_type
+
+--, sum(gross_pickup_request) gross_pickup
+--, sum(gross_pickup_done) gross_pickup_done
+--, sum(gross_pickup_failed) gross_pickup_failed
+--, sum(ontime_pickup_done) gross_ontime_PU_done
+--, sum(ontime_pickup) gross_ontime_PU
+, round(approx_percentile(((to_unixtime(pku_1st_time) - f.Logistics_request_created)/86400),0.95),3) gap_time
+-- , cast(sum(gross_pickup_done) as DOUBLE)/ sum(gross_pickup_request) rate_pickup_done
+-- , avg(pickup_leadtime) avg_pickup_leadtime
+
+FROM (select * from mp_order.dwd_order_all_ent_df__vn_s0_live where grass_date > date '2022-07-01') o 
+left join Final f on f.order_id = o.order_id
+WHERE 
+--o.fulfilment_shipping_carrier in ('VNPost Nhanh','VNPost Tiết Kiệm')
+--and
+ f.shipment_type1 = 'pickup'
+and date(scheduled_date1) between date '2022-10-01' and date '2022-10-31'
+and o.seller_shipping_address_state in ('Hà Nội','TP.Hồ Chí Minh')
+GROUP BY 1,2,3
