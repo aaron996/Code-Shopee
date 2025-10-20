9201f5c2c597fae80688e4d10affc86cc0a6c374
aaron996 <35775571+aaron996@users.noreply.github.com>
2025-10-20 14:24:40 +0700

    Add query for exporting reverse cancel order data
    Added a new query to export data for reverse cancel orders.

--- GHN_Code xử lý mail M14 (C3 TiKi).sql
+++ GHN_Code xử lý mail M14 (C3 TiKi).sql
@@ -23,18 +23,19 @@ WHERE clientid = 224845
   AND deliverywarehouseid in (1327,21965000)
 
 
--- code chạy mã đơn trong email
 
-SELECT
-    C.ordercode AS MaDH
-    ,S.clientid AS LoaiDonHang
-    ,DATE(C.orderdate) AS NgayTaoDon
-    ,C.currentstatus AS TrangThai
-    ,C.numpick AS SoLanLay
-  FROM "gsheet-data_input_from_external"."default"."input_customer_shopee" GS
-  JOIN "ghn-reporting"."ka"."dtm_ka_v3_createddate" C
-    ON C.ordercode = GS."OrderCode"
-  JOIN "ghn-reporting"."ka"."dtm_ka_shopee" AS S
-    ON C.ordercode = S.ordercode
-  WHERE GS."OrderCode" IS NOT NULL
+-- Export data reverse cancel
+SELECT 
+  GS."OrderCode"
+  ,clientid
+  ,DATE(enddeliverytime) AS "enddeliverytime"
+  ,DATE(lastdeliveredupdatedtime) AS "lastdeliveredupdatedtime"
+  ,currentstatus AS Status
+  ,numdeliver
+  ,numreturn
+  ,currentwh
+FROM "gsheet-data_input_from_external"."default"."input_customer_shopee" GS
+LEFT JOIN "ghn-reporting"."ka".dtm_ka_v3_createddate C
+  ON GS."OrderCode" = C.ordercode
+WHERE GS."OrderCode" IS NOT NULL
 
